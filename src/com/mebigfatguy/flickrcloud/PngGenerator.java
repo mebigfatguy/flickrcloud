e2735ee4897eb99e178a0251e6cf50098bf0c67f
Dave Brosius <dbrosius@mebigfatguy.com>
2013-09-30 21:56:10 -0400

    start writing directory zips

--- src/com/mebigfatguy/flickrcloud/PngGenerator.java
+++ src/com/mebigfatguy/flickrcloud/PngGenerator.java
@@ -17,14 +17,21 @@
  */
 package com.mebigfatguy.flickrcloud;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class PngGenerator {
 
-    public Map<String, File> generate(List<File> transferData) {
+    public Map<String, File> generate(List<File> transferData) throws IOException {
         Map<String, File> images = new HashMap<String, File>();
         for (File sourceFile : transferData) {
             File f;
@@ -42,8 +49,48 @@ public class PngGenerator {
         return images;
     }
     
-    private File createDirectoryZip(File directory) {
-        return null;
+    private File createDirectoryZip(File directory) throws IOException {
+        File zipFile = File.createTempFile("zip", ".zip");
+        zipFile.deleteOnExit();
+        
+        try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zipFile)))) {
+            
+            zos.setLevel(9);
+            zos.setMethod(ZipOutputStream.DEFLATED);
+
+            addChildFile(zos, directory, directory.getPath().length() + 1);              
+        }
+        
+        return zipFile;
+    }
+    
+    private void addChildFile(ZipOutputStream zos, File child, int rootPathLength) throws IOException {
+        
+        boolean isRoot = (rootPathLength >= child.getPath().length());
+        if (!isRoot) {
+            String relativePath = child.getPath().substring(rootPathLength);
+            ZipEntry ze = new ZipEntry(relativePath);
+            zos.putNextEntry(ze);
+        }
+        
+        if (child.isDirectory()) {
+            for (File subChild : child.listFiles()) {
+                addChildFile(zos, subChild, rootPathLength);
+            }
+        } else {
+            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(child))) {
+                byte[] data = new byte[2048];
+                int size = bis.read(data);
+                while (size >= 0) {
+                    zos.write(data, 0, size);
+                    size = bis.read(data);
+                }
+            }
+        }
+        
+        if (!isRoot) {
+            zos.closeEntry();
+        }
     }
     
     private File createImageFile(File file) {
